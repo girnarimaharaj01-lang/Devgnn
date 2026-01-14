d80d3934596b0ab54c6a2ac00a8ecf024cd4712f
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-14 15:23:02 +0530

    Update config.py with default environment values

--- config.py
+++ config.py
@@ -3,10 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+API_ID = int(getenv("API_ID", "29777466"))
+API_HASH = getenv("API_HASH", "a04b3df726520026f207079aec2f9879")
+BOT_TOKEN = getenv("BOT_TOKEN", "8346695576:AAErSTkMH1Jcb-yK0nE2gKCuSorGg0OhQMI")
+OWNER_ID = int(getenv("OWNER_ID", "8399557684"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
 FORCESUB = getenv("FORCESUB", "")
+
